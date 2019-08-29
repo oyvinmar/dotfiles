f918e4a8aacbe68c324d443eff2f204283844a9d
Øyvind Marthinsen <oyvinmar@gmail.com>
2019-08-29 07:11:19 +0000

    update SHELL export for other scripts

--- init/40_zsh.sh
+++ init/40_zsh.sh
@@ -1,4 +1,7 @@
-# set zsh as default shell
-if [[ "$SHELL" != *zsh ]]; then
+
+# Set the default shell to zsh if it isn't currently set to zsh
+if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
   chsh -s $(which zsh)
+  # Update shell export for other scripts (i.e. volta)
+  export SHELL=$(which zsh)
 fi
