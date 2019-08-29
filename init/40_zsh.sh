f7a6fc1c87cafd31061e16c1d2d69f9d43e0bb22
Øyvind Marthinsen <oma@knowit.no>
2019-08-29 12:32:41 +0200

    fix zsh issue in dotfiles script

--- init/40_zsh.sh
+++ init/40_zsh.sh
@@ -1,6 +1,6 @@
 
 # Set the default shell to zsh if it isn't currently set to zsh
-if [[ ! $(echo $SHELL) == $(which zsh) ]]; then
+if ! is_zsh; then
   chsh -s $(which zsh)
   # Update shell export for other scripts (i.e. volta)
   export SHELL=$(which zsh)
