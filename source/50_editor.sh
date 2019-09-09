85ae5a44f7088660974418c41f75b85f387275e2
Øyvind Marthinsen <oyvinmar@gmail.com>
2019-09-09 10:07:50 +0000

    fix mvim check

--- source/50_editor.sh
+++ source/50_editor.sh
@@ -3,7 +3,7 @@
 export EDITOR=nvim
 
 # If mvim is installed, use it instead of native vim
-if [[ "$(which mvim)" ]]; then
+if [[ "$(which mvim)" =~ mvim$ ]]; then
    EDITOR="mvim -v"
    alias vim="$EDITOR"
 fi
