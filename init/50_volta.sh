22a3e823c76e91290ed91558089900a73bd91f92
Øyvind Marthinsen <oyvinmar@gmail.com>
2019-09-18 21:09:24 +0200

    diff volta tool install

--- init/50_volta.sh
+++ init/50_volta.sh
@@ -4,8 +4,13 @@ tools=(
   node
   yarn
   serve
+  now
 )
 
+# For some reason we can only pipe the list when the --verbose flag is set
+installed_tools="$(~/.volta/volta list --format=plain --verbose | awk '/ .*@.* / {print $2}' | sed 's/@.*//g')"
+
+tools=($(setdiff "${tools[*]}" "${installed_tools[*]}"))
 for tool in "${tools[@]}"; do
   ~/.volta/volta install $tool
 done
