d7c6947ba48f7fdee6a71ec93937e119c32d7e93
Øyvind Marthinsen <oma@knowit.no>
2019-09-27 15:47:51 +0200

    update nignx letsencrypt conf

--- conf/setup/setup-nginx-letsencrypt.sh
+++ conf/setup/setup-nginx-letsencrypt.sh
@@ -4,5 +4,5 @@ sudo apt install nginx python-certbot-nginx
 
 sudo curl -fsSL https://raw.githubusercontent.com/oyvinmar/dotfiles/master/conf/setup/nginx.conf > /etc/nginx/sites-available/default
 
-sudo certbot --nginx -d dev.example.com, example.com
+sudo certbot certonly --manual -d "*.dev.example.com" -d dev.example.com --preferred-challenges=dns
 sudo systemctl reload nginx
