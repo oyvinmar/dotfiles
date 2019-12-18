7789fc36a422ea268a64a0bd13f897435a4177b1
Øyvind Marthinsen <oma@knowit.no>
2019-12-18 13:48:09 +0100

    enable cask list

--- init/30_macos_homebrew_casks.sh
+++ init/30_macos_homebrew_casks.sh
@@ -11,19 +11,29 @@ brew cask info this-is-somewhat-annoying 2>/dev/null
 # Homebrew casks
 casks=(
   # Applications
-  # alfred
-  # bartender
-  # docker
-  # dropbox
-  # google-chrome
-  # google-chrome-canary
-  # visual-studio-code
-  # firefox
-  # iterm2
-  # macvim
-  # slack
-  # spotify
-  # the-unarchiver
+   1password
+   alfred
+   bartender
+   docker
+   dropbox
+   kap
+   google-chrome
+   google-chrome-canary
+   hyper
+   visual-studio-code
+   firefox
+   insomnia
+   iterm2
+   numi
+   omnifocus
+   macvim
+   slack
+   spotify
+   pastebot
+   the-unarchiver
+   tweetbot
+   simplenote
+   whatsapp
 )
 
 # Install Homebrew casks.
