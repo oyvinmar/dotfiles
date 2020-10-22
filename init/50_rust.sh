8637b25fb79247ec9d6b25dc946341c445050062
Øyvind Marthinsen <oyvinmar@gmail.com>
2020-10-22 19:20:26 +0200

    add rust

---
+++ init/50_rust.sh
@@ -0,0 +1,6 @@
+if [[ ! "$(type -P rustup)" ]]; then
+  echo "Installing rust"
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > install-rust.sh
+  chmod +x install-rust.sh
+  ./install-rust.sh --no-modify-path --profile default -y
+fi
--- source/01_path.sh
+++ source/01_path.sh
@@ -1,1 +1,1 @@
-export PATH="$DOTFILES/bin:$HOME/.bin:$PATH"
+export PATH="$DOTFILES/bin:$HOME/.cargo/bin:$HOME/.bin:$PATH"
