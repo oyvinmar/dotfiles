9a8095ed11ef24df27fa6ce4830a9f11ebee199b
Øyvind Marthinsen <oyvinmar@gmail.com>
2021-01-07 14:30:36 +0100

    remove cask applications

--- init/30_macos_homebrew_casks.sh
+++ init/30_macos_homebrew_casks.sh
@@ -7,41 +7,43 @@ is_macos || return 1
 
 # Hack to show the first-run brew-cask password prompt immediately.
 brew cask info this-is-somewhat-annoying 2>/dev/null
+brew tap homebrew/cask-versions
 
 # Homebrew casks
 casks=(
+  adoptopenjdk8
   # Applications
-   1password
-   alfred
-   bartender
-   docker
-   kap
-   google-chrome
-   hyper
-   visual-studio-code
-   firefox
-   insomnia
-   iterm2
-   numi
-   omnifocus
-   macvim
-   slack
-   spotify
-   pastebot
-   the-unarchiver
-   simplenote
-   whatsapp
-   fantastical
-   rocket
-   discord
+  #  1password
+  #  alfred
+  #  bartender
+  #  docker
+  #  kap
+  #  google-chrome
+  #  hyper
+  #  visual-studio-code
+  #  firefox
+  #  insomnia
+  #  iterm2
+  #  numi
+  #  omnifocus
+  #  macvim
+  #  slack
+  #  spotify
+  #  pastebot
+  #  the-unarchiver
+  #  simplenote
+  #  whatsapp
+  #  fantastical
+  #  rocket
+  #  discord
 )
 
 # Install Homebrew casks.
-casks=($(setdiff "${casks[*]}" "$(brew cask list 2>/dev/null)"))
+casks=($(setdiff "${casks[*]}" "$(brew list --casks 2>/dev/null)"))
 if (( ${#casks[@]} > 0 )); then
   e_header "Installing Homebrew casks: ${casks[*]}"
   for cask in "${casks[@]}"; do
-    brew cask install $cask
+    brew install --cask $cask
   done
   brew cask cleanup
 fi
