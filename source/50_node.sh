59fb133401879625dd286d1744f7766fbd7588f1
Øyvind Marthinsen <oyvinmar@gmail.com>
2021-08-29 21:18:27 +0200

    alias build and watch from dev-cli

--- source/50_node.sh
+++ source/50_node.sh
@@ -7,6 +7,8 @@ alias yb='yarn build'
 alias yd='yarn dev'
 alias yui='yarn upgrade-interactive --latest'
 alias dev='dev-cli dev'
+alias build='dev-cli build'
+alias watch='dev-cli watch'
 
 function nr () {
   npm run $1 -- $@[2,-1]
