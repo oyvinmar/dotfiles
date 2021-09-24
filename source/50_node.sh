5329ce5598525193c569fbc2150e14e217e97a78
Øyvind Marthinsen <oyvinmar@gmail.com>
2021-09-24 09:00:34 +0200

    more nel aliases

--- source/50_node.sh
+++ source/50_node.sh
@@ -9,8 +9,9 @@ alias yui='yarn upgrade-interactive --latest'
 alias dev='nel dev'
 alias build='nel build'
 alias watch='nel watch'
+alias deploy='nel deploy'
 alias cb='nel vc co'
-alias cpr='nel vc cop'
+alias copr='nel vc cop'
 alias vc='nel vc'
 
 function nr () {
