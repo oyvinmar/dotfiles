d108414e6490dbb396d8a64b7cad98e2e48e5110
Øyvind Marthinsen <EMAIL>
2022-04-03 19:48:23 +0200

    install difftastic

--- init/50_rust.sh
+++ init/50_rust.sh
@@ -4,3 +4,12 @@ if [[ ! "$(type -P rustup)" ]]; then
   chmod +x install-rust.sh
   ./install-rust.sh --no-modify-path --profile default -y
 fi
+
+crates=(
+  difftastic
+)
+
+for crate in "${crates[@]}"; do
+  cargo install $crate
+done
+
