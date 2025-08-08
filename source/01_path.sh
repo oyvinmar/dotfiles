1e5063512a94a1e8d3bc491700257834f7e8b062
Øyvind Marthinsen <oyvinmar@gmail.com>
2025-08-08 10:21:27 +0200

    Add to path

--- source/01_path.sh
+++ source/01_path.sh
@@ -1,4 +1,4 @@
-export PATH="$DOTFILES/bin:$HOME/.cargo/bin:$HOME/.bin:$PATH"
+export PATH="$DOTFILES/bin:$HOME/.cargo/bin:$HOME/.bin:$PATH":$HOME/.local/bin
 
 if is_macos_arm; then
   export PATH="/opt/homebrew/bin:$PATH"
